4a3f3763a1268d1beec48eaff9b8d54cdb6f3fea
Garnet-Things <garnetthings@gmail.com>
2025-09-07 10:03:21 +0530

    garnet: Add dts extract algorithm

--- extract.sh
+++ extract.sh
@@ -1,2 +1,13 @@
+#!/bin/bash
+
+# Extract DTBS
+python3 -m venv my-env
+source my-env/bin/activate
+pip3 install extract-dtb
+extract-dtb dtbo.img -o ./dts/
+
+# Extract kernel
 python3 ../../../system/tools/mkbootimg/unpack_bootimg.py --boot_img boot.img --out ./
-rm -rf boot.img boot_signature ramdisk
+
+# Clean Up
+rm -rf ramdisk my-env boot_signature boot.img dtbo.img ./dts/00_kernel
